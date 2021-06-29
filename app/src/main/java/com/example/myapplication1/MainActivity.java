222a1e9675a6ee03899be17110880e5c54141d73
RJ <bulychev.arsenya@mail.ru>
2021-06-29 09:47:51 +0300

    Exam

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.myapplication1.R;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    EditText etNum1;
+    EditText etNum2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    TextView tvResult;
+
+    String oper = "";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // находим элементы
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+        //Пустота
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
+
+        //читка едит текста
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+
+        // определить операцию
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+
+        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
+    }
+
+}
